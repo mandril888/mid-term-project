4382d150d839eab321ce4d92a5e686c15a389107
Arturo Rubio <arubio@seocom.agency>
2022-08-30 20:52:27 +0200

    navbar finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-    console.log('hi');
+
+    // add class to an element if isn't on top
+    function addActiveClassNavbar(element, className) {
+        let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrollPosition) element.classList.add(className);
+        else element.classList.remove(className);
+    }
+
+    const navbar = document.querySelector('.navbar');
+    addActiveClassNavbar(navbar, 'active');
+    window.addEventListener('scroll', () => {
+        addActiveClassNavbar(navbar, 'active');
+    });
 
 });
