c1c54b6e82089d9d8f01cc52158df30367756e7a
Arturo Rubio <arubio@seocom.agency>
2022-08-31 17:34:25 +0200

    projects home finished

--- src/js/script.js
+++ src/js/script.js
@@ -7,10 +7,31 @@ document.addEventListener('DOMContentLoaded', () => {
         else element.classList.remove(className);
     }
 
+    // add 'active' class to navbar on scroll
     const navbar = document.querySelector('.navbar');
     addActiveClassNavbar(navbar, 'active');
     window.addEventListener('scroll', () => {
         addActiveClassNavbar(navbar, 'active');
+    });
+
+    // smooth scroll to an element
+    function scrollIntoViewWithOffset(element, offset) {
+        window.scrollTo({
+            behavior: 'smooth',
+            top:
+                document.querySelector(element).getBoundingClientRect().top -
+                document.body.getBoundingClientRect().top -
+                offset,
+        })
+    }
+
+    // smoth sroll on click # anchor
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(event) {
+            event.preventDefault();
+            const idElement = this.getAttribute('href');
+            scrollIntoViewWithOffset(idElement, 0);
+        });
     });
 
 });
