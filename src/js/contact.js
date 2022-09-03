f9f60b2161be718cc4d0ddd3ce2e932ab8ed08aa
Arturo Rubio <arubio@seocom.agency>
2022-09-03 16:52:43 +0200

    contact form POST data to Deta finished

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,10 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    // const urlToPostDeta = 'https://database.deta.sh/v1/a0wwnrex/contactmessages/items';      // lab
+    const urlToPostDeta = 'https://database.deta.sh/v1/a0fgw0th/contactmessages/items';         // personal
+
+    // const xApiKeyDeta = 'a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H';                         // lab
+    const xApiKeyDeta = 'a0fgw0th_qJnhonBfy71SQWGdN8fp3s4TwQdv5soz';                            // personal
+
+    // auto load data form
+    // document.getElementById('name').value = 'Arturo';
+    // document.getElementById('email').value = 'test@test.com';
+    // document.getElementById('phone').value = '+12345678911';
+    // document.getElementById('message').value = 'lheiji piuhp eidu piu plheiji piuhp eidu piu plheiji piuhp eidu piu plheiji piuhp eidu piu p';
+
+    
     // show alert of an element
     function showAlert(element, alertText) {
         element.classList.remove('hide');
         element.classList.add('appear');
-        element.innerHTML = alertText;
+        if (alertText) element.innerHTML = alertText;
     }
     
     // hide alert of an element
@@ -17,23 +30,53 @@ document.addEventListener('DOMContentLoaded', () => {
     // fields validation
     function fieldsValidation(element) {
         const siblingAlert = element.nextSibling.nextSibling;
-
-        if (!element.value) showAlert(siblingAlert, 'This field is required.');
+        let returnValueBool = true;
+        
+        if (!element.value) {
+            showAlert(siblingAlert, 'This field is required.');
+            returnValueBool = false;
+        }
         else hideAlert(siblingAlert);
         
         const mailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w+([\.-]?\w+))+$/;
         if (element.type === 'email' && element.value && !mailPattern.test(element.value)) {
             showAlert(siblingAlert, 'Invaild email.');
+            returnValueBool = false;
         }
         
         const telPattern = /^\+([0-9]{11})$/;
         if (element.type === 'tel' && element.value && !telPattern.test(element.value)) {
             showAlert(siblingAlert, 'Invalid phone, must start with + and have 11 characters without spaces.');
+            returnValueBool = false;
         }
 
         if (element.type === 'textarea' && element.value && element.value.length < 50) {
             showAlert(siblingAlert, 'This field must have at least 50 characters.');
+            returnValueBool = false;
         }
+
+        return returnValueBool;
+    }
+
+    // send data to Deta endpoint
+    function sendData(data) {
+        const url = urlToPostDeta; 
+        const body = { item: data };
+        const fetchParams = {
+            method: 'POST',
+            headers: {
+                "Accept": 'application/json',
+                "Content-Type": 'application/json',
+                "X-API-Key": xApiKeyDeta
+            },
+            body: JSON.stringify(body)
+        };
+    
+        fetch(url, fetchParams)
+            .then(response => {
+                if (response.ok) showAlert(document.querySelector('.contact__sent'));
+                else showAlert(document.querySelector('.contact__error'));
+            })
     }
 
     // show or hide alerts on submit form for required fields
@@ -41,9 +84,26 @@ document.addEventListener('DOMContentLoaded', () => {
     if (submitButton) {
         submitButton.addEventListener('click', (e) => {
             e.preventDefault();
+
+            let sendDataBool = true;
+
             document.querySelectorAll('.required').forEach(item => {
-                fieldsValidation(item);
+                const validation = fieldsValidation(item);
+                if (!validation) sendDataBool = false;
             });
+
+            if (sendDataBool) {
+                const form = document.querySelector('.contact__form');
+                const formElements = Array.from(form.elements); 
+                let datos = {};
+                
+                for (let i=0; i < formElements.length-1; i++) {
+                    datos[formElements[i].id] = formElements[i].value;
+                }
+
+                form.reset();
+                sendData(datos);
+            }
         });
     }
     
